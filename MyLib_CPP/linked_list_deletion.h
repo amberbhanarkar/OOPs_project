7b39a79ed5d722f6d14dc44e7496060fba617e49
Amber Bhanarkar <amberbhanarkar@gmail.com>
2018-11-19 02:24:43 +0530

    Deletion in singly linked list

---
+++ MyLib_CPP/linked_list_deletion.h
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <cstdlib>
+using namespace std; 
+struct Node 
+{ 
+    int data; 
+    struct Node *next; 
+}; 
+void push(struct Node** head_ref, int new_data) 
+{ 
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+    new_node->data  = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref)    = new_node; 
+} 
+void deleteNode(struct Node **head_ref, int key) 
+{ 
+    struct Node* temp = *head_ref, *prev; 
+    if (temp != NULL && temp->data == key) 
+    { 
+        *head_ref = temp->next;   
+        free(temp);               
+        return; 
+    } 
+    while (temp != NULL && temp->data != key) 
+    { 
+        prev = temp; 
+        temp = temp->next; 
+    } 
+    if (temp == NULL) return; 
+    prev->next = temp->next; 
+  
+    free(temp);  
+} 
+void printList(struct Node *node) 
+{ 
+    while (node != NULL) 
+    { 
+        cout<<node->data; 
+        node = node->next; 
+    } 
+} 
+  
